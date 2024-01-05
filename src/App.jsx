febea3e67979ece054cf432877e4312e5b720621
nGhart <naaghart@gmail.com>
2024-01-05 18:07:06 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Privacy Policy</h1>
+        <h4>Effective date: January 4, 2023</h4>
+        <p>
+          This privacy policy notice for Cookiez beads N more ("us", "we", or
+          "our") operates the <a href="https://order-list-vert.vercel.app/"></a>{" "}
+          and how we might collect, store, use and/or share ("process") your
+          information when you use our (the "Services").
+        </p>
+        <p>
+          <strong>Questions and concerns?</strong> Reading this privacy notice
+          will help you understand your privacy rights and choices. If you do
+          not agree with our policies and practices, please do not use our
+          Services. If you still have any questions contact us at{" "}
+          <a href="mailto:naaghwork@gmail.com">naaghwork@gmail.com</a>.
+        </p>
+
+        <h3>WHAT INFORMATION DO WE COLLECT</h3>
+        <p>
+          We collect personal information that you voluntarily provide to us
+          when you express an interest in obtaining information about us or our
+          products and Services.
+        </p>
+        <p>The personal information we collect may include the following :</p>
+        <ul>
+          <li>names</li>
+          <li>email addresses</li>
+          <li>passwords</li>
+          <li>phone numbers</li>
+        </ul>
+        <p>
+          <strong>Sensitive Information</strong> When necessary, with your
+          consent or as otherwise permitted by applicable law, we process the
+          following categories of sensitive information.
+        </p>
+      </div>
+      <h3>HOW DO WE PROCESS YOU INFORMATION</h3>
+      <p>We process your information to provide and administer our Services.</p>
+      <h3>HOW LONG DO WE KEEP YOUR INFORMATION</h3>
+      <p>
+        We may share information in specific situations described in this
+        section and/or with third parties.
+      </p>
+      <p></p>
+    </>
+  );
+}
+
+export default App;
